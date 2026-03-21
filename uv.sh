1d5d511cdad34c71fb9e72704877d39c3194b3d1
msleigh <msleigh@users.noreply.github.com>
2026-03-21 19:36:42 +0000

    uv.sh: add uv.sh file

---
+++ uv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using uv
+
+# LLM (https://llm.datasette.io)
+uv tool install llm
+
+# Pre-Commit (https://pre-commit.com/)
+uv tool install pre-commit
+
+# Ruff (https://docs.astral.sh/ruff)
+uv tool install ruff
